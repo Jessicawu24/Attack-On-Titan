a30e00141fcdd893cf4d3b5b5f1aada934936086
Jessicawu24 <44040575+Jessicawu24@users.noreply.github.com>
2018-12-12 17:52:51 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,9 @@
+    $(".Chomp").click(function() {
+        $(".One1").fadeToggle();
+});
+    $(".Charge").click(function() {
+        $(".Two2").slideToggle();
+}); 
+$(".Transform").click(function() {
+        $(".Three3").toggle();
+}); 
